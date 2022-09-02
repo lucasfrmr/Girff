135c78a4e310e3a474ea27ff31cf0dda24f9c33e
Lucas <lucasfrmr@gmail.com>
2022-09-02 08:45:21 -0500

    Bruh

--- content.js
+++ content.js
@@ -1,14 +1,6 @@
-console.log("Bruh!");
+console.log("Bruh");
 
 let pd = document.getElementsByClassName("prodDetAttrValue");
 console.log(pd[0].textContent())
 pd[0].style['background-color'] = '#eb4034';
 console.log(pd);
-
-
-// let ts = document.getElementsByClass("prodDetAttrValue");
-// ts[3].style['background-color'] = '#eb4034';
-// console.log("Bruh...");
-
-// Temp install to
-//     about:debugging
